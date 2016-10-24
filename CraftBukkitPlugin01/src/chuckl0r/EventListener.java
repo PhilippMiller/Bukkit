5cf871e04926d2265ee0e445505b715611dfbb03
PhilippMiller <philipp.miller@gmx.de>
2016-10-24 21:30:00 +0200

    remove unimportant import

--- CraftBukkitPlugin01/src/chuckl0r/EventListener.java
+++ CraftBukkitPlugin01/src/chuckl0r/EventListener.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.World;
